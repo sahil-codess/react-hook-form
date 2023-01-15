629ae56e8d7fd7b05a361dfccd6a5d7301aa58b4
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-15 09:23:43 +0430

    made some changes

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,14 +1,21 @@
 import { useForm } from "react-hook-form";
-import React from "react";
 
 export default function App() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
 
+  console.log(watch("example")); // watch input value by passing the name of it
+
   return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder='Email' name='email' ref={register} />
-      <input type="password" placeholder='password' name='password' ref={register} />
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+      
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
       
       <input type="submit" />
     </form>
