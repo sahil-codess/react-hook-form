312ba639a8112345d070597dfa845809063faf7f
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-15 23:28:13 +0430

    in the initial render of the array in console the values are undefined and this is because the watchAPI is a function and we can provide default values to it to prevent this behavior

--- src/Components/WatchAPI.js
+++ src/Components/WatchAPI.js
@@ -2,21 +2,26 @@ import * as React from "react";
 import { useForm } from "react-hook-form";
 
 export default function App() {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch } = useForm({
+    defaultValues: {
+        firstName: 'Qais',
+        lastName: 'Erfan',
+        age: '23'
+    }
+  });
 
   const onSubmit = data => {
     console.log(data)
   }
 
-  console.log(watch('lastName'))
+  console.log(watch('firstName'))
 
   return (
     <div>
         <form onSubmit={handleSubmit(onSubmit)}>
           <input {...register('firstName')} placeholder="First name" />
-
           <input {...register('lastName')} placeholder="Last name" />
-
+          <input type="number" {...register("age")} placeholder="Age" />
           <input type="submit" />
         </form>
     </div>
