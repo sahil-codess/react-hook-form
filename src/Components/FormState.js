77b97ef781d7d20c9997811c0a21817ede39f624
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-16 10:40:10 +0430

    touchedFeilds checks wheather user touched or focused the input feild

--- src/Components/FormState.js
+++ src/Components/FormState.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useForm } from "react-hook-form";
 
 export default function App() {
-  const { register, handleSubmit, formState:{dirtyFields}} = useForm({
+  const { register, handleSubmit, formState:{touchedFields}} = useForm({
     defaultValues: {
         firstName: '',
         lastName: '',
@@ -10,7 +10,7 @@ export default function App() {
     }
   });
 
-  console.log("dirtyFields", dirtyFields)
+  console.log("touchedFields", touchedFields)
 
   return (
     <div>
