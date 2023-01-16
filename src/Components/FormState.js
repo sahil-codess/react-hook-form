a7c783ae537de5259dc461c0dde8df5c80301803
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-16 10:41:57 +0430

    isSubmitted checks whether the form is being submitted regardless of the form input validations

--- src/Components/FormState.js
+++ src/Components/FormState.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useForm } from "react-hook-form";
 
 export default function App() {
-  const { register, handleSubmit, formState:{touchedFields}} = useForm({
+  const { register, handleSubmit, formState:{isSubmitted}} = useForm({
     defaultValues: {
         firstName: '',
         lastName: '',
@@ -10,7 +10,7 @@ export default function App() {
     }
   });
 
-  console.log("touchedFields", touchedFields)
+  console.log("isSubmitted", isSubmitted)
 
   return (
     <div>
