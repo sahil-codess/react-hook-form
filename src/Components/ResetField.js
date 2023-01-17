e9eb5e08d96c5e016fc2a01886657bdb5b42f487
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-17 13:03:28 +0430

    onChange mode prints the error messages in type

--- src/Components/ResetField.js
+++ src/Components/ResetField.js
@@ -2,17 +2,23 @@ import * as React from "react";
 import { useForm } from "react-hook-form";
 
 export default function App() {
-  const { register, handleSubmit, resetField ,formState:{errors}} = useForm({
+  const { register, handleSubmit, resetField ,formState:{errors, isValid}} = useForm({
     defaultValues: {
         firstName: 'Qais',
-    }
+    },
+    mode: "onChange"
   });
 
   return (
     <div>
         <form onSubmit={handleSubmit((data) => console.log(data))}>
-          <input {...register('firstName',{required: true})} placeholder="First name" />
-          <button type="button" onClick={() => resetField("firstName")}>Reset Field</button>
+          <input {...register('firstName',{required: 'This is required', minLength: {
+            value: 6,
+            message: "min lenght is 6"
+          }})} placeholder="First name" />
+          <p>{errors.firstName?.message}</p>
+          <p>{isValid ? 'Valid' : 'Not Valid'}</p>
+          <button type="button" onClick={() => resetField("firstName", { keepError: true})}>Reset Field</button>
           <input type="submit" />
         </form>
     </div>
