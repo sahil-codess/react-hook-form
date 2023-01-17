cfc7e7d5018539900fc781535663adf37677e5bc
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-17 10:08:46 +0430

    multiple setErrors

--- src/Components/SetError.js
+++ src/Components/SetError.js
@@ -5,6 +5,7 @@ export default function App() {
   const { register, handleSubmit, setError, formState: {errors}} = useForm({
     defaultValues: {
         firstName: '',
+        criteriaMode: "all"
     }
   });
 
@@ -13,8 +14,14 @@ export default function App() {
     <div>
         <form onSubmit={handleSubmit((data) => console.log(data))}>
           <input {...register('firstName')} placeholder="First name" />
-           <button onClick={() => setError("firstName", { type: "custom", message: 'Something is wrong'})}>set Error</button>
-            <p>{errors.firstName?.message}</p>
+           <button onClick={() => setError("firstName",
+            { types: {
+                test: 'test is wrong',
+                test1: 'test1 is wrong'
+            }})}>
+            set Error</button>
+            <p>{errors.firstName?.types?.test}</p>
+            <p>{errors.firstName?.types?.test1}</p>
           <input type="submit" />
         </form>
     </div>
