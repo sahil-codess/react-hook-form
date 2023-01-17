f0e27d485b47ed5fb535b30b996df6a9958461eb
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-17 13:09:04 +0430

    checked the dirty and isDirty APIs

--- src/Components/ResetField.js
+++ src/Components/ResetField.js
@@ -2,13 +2,14 @@ import * as React from "react";
 import { useForm } from "react-hook-form";
 
 export default function App() {
-  const { register, handleSubmit, resetField ,formState:{errors, isValid}} = useForm({
+  const { register, handleSubmit, resetField ,formState:{isDirty, dirtyFields}} = useForm({
     defaultValues: {
-        firstName: 'Qais',
+        firstName: '',
     },
     mode: "onChange"
   });
 
+  console.log("isDirty, dirtyFields", isDirty, dirtyFields)
   return (
     <div>
         <form onSubmit={handleSubmit((data) => console.log(data))}>
@@ -16,9 +17,8 @@ export default function App() {
             value: 6,
             message: "min lenght is 6"
           }})} placeholder="First name" />
-          <p>{errors.firstName?.message}</p>
-          <p>{isValid ? 'Valid' : 'Not Valid'}</p>
-          <button type="button" onClick={() => resetField("firstName", { keepError: true})}>Reset Field</button>
+          
+          <button type="button" onClick={() => resetField("firstName", { keepDirty: true})}>Reset Field</button>
           <input type="submit" />
         </form>
     </div>
